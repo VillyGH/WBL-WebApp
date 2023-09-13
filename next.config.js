c03de14943397aa7f490208466c148444cda2e7e
William Blanchet Lafrenière <willclash@outlook.com>
2023-09-13 15:17:24 -0400

    remove conflicts

--- next.config.js
+++ next.config.js
@@ -1,9 +1,4 @@
 module.exports = {
-<<<<<<< Updated upstream
-    output: 'export',
-    assetPrefix: process.env.NODE_ENV === 'production' ? '/WBL-WebApp/' : undefined,
-}
-=======
     images: {
         loader: 'imgix',
         path: '/',
@@ -11,4 +6,3 @@ module.exports = {
     output: process.env.NODE_ENV === 'production' ? 'export' : undefined,
     assetPrefix: process.env.NODE_ENV === 'production' ? '/WBL-WebApp/' : undefined,
 }
->>>>>>> Stashed changes
