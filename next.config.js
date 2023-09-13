dc3b15b54c996a81571c1fb1bb3e917b3bef6403
William Blanchet Lafrenière <willclash@outlook.com>
2023-09-13 11:20:07 -0400

    remove image optimisation

--- next.config.js
+++ next.config.js
@@ -1,18 +1,3 @@
-const withPlugins = require('next-compose-plugins');
-const optimizedImages = require('next-optimized-images');
-module.exports = withPlugins([
-    [optimizedImages, {
-        mozjpeg: {
-            quality: 80,
-        },
-        pngquant: {
-            speed: 3,
-            strip: true,
-            verbose: true,
-        },
-        imagesPublicPath: '/WBL-WebApp/_next/static/images/',
-    }],
-    {
-        assetPrefix: process.env.NODE_ENV === 'production' ? '/WBL-WebApp/' : undefined,
-    },
-]);
+module.exports = {
+    assetPrefix: process.env.NODE_ENV === 'production' ? '/WBL-WebApp/' : undefined,
+}
