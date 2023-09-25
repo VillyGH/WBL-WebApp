d18d0ee9872b64dc8a627377235801e6df8ee870
William Blanchet Lafrenière <willclash@outlook.com>
2023-09-25 02:29:30 -0400

    config

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ export default {
     output: {
         path: path.join(__dirname, "build"),
         filename: "[name].bundle.js",
-        publicPath: process.env.ENV_TYPE != undefined ? '/WBL-WebApp/' : "/"
+        publicPath: process.env.BASE_URL
     },
     mode: process.env.NODE_ENV || "development",
     resolve: {
