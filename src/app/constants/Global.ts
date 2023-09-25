6539e51dbbd34da94bdd79e21b6f8befb144d3fe
William Blanchet Lafrenière <willclash@outlook.com>
2023-09-25 01:58:15 -0400

    config

--- src/app/constants/Global.ts
+++ src/app/constants/Global.ts
@@ -1,2 +1,2 @@
 export let APP_NAME: string = "WBL-WebApp"
-export let BASE_URL: string | undefined = process.env.NODE_ENV === 'production' ? '/WBL-WebApp/' : undefined
+export let BASE_URL: string | undefined = process.env.NODE_ENV === 'development' ? '/' : '/WBL-WebApp/'
