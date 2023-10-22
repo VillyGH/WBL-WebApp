02b2872fb5a65ffeb91bf93c07fa1abf824f1658
William Blanchet Lafrenière <willclash@outlook.com>
2023-10-22 14:39:54 -0400

    update packages + Contact server

--- src/app/pages/Contact.tsx
+++ src/app/pages/Contact.tsx
@@ -10,10 +10,75 @@ import {faFacebook, faGithub, faReddit} from "@fortawesome/free-brands-svg-icons
 import {ParticlesOptsDark} from "../types/ParticlesDark";
 import {ParticlesOpts} from "../types/Particles";
 
-export class Contact extends React.Component {
+interface State {
+    name: string,
+    email: string,
+    message: string
+}
+
+export class Contact extends React.Component<unknown, State> {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            name: "",
+            email: "",
+            message: ""
+        };
+    }
     public componentDidMount() {
         document.title = "Me contacter - " + APP_NAME;
     }
+
+    readonly #handleChange = (event: React.ChangeEvent<HTMLFormElement>): void => {
+        const target = event.target;
+        const value = target.type === "checkbox" ? target.checked : target.value;
+        const name = target.name;
+
+        if (!name) {
+            throw new Error("Name is undefined for element in form.");
+        }
+
+        this.setState({
+            ...this.state, ...{
+                [name]: value,
+            }
+        });
+    };
+
+    readonly #handleSubmit = async (event: React.FormEvent<HTMLFormElement>): Promise<void> => {
+        const form = event.currentTarget;
+        let isValid = form.checkValidity();
+
+        event.preventDefault();
+        event.stopPropagation();
+
+        if (isValid) {
+            const data = {
+                name: this.state.name,
+                email: this.state.email,
+                message: this.state.message
+            };
+
+            try {
+                const response = await fetch("/send-email", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify(data)
+                });
+
+                if (response.status === 200) {
+                    alert("Courriel envoyé avec succès !");
+                } else {
+                    alert("Erreur lors de l'envoi du courriel.");
+                }
+            } catch (error) {
+                console.error("Erreur lors de la requête d'envoi de courriel :", error);
+            }
+        }
+    };
 
     public render(): ReactElement | null {
         return (
@@ -24,20 +89,36 @@ export class Contact extends React.Component {
                         <div className="me-4 mt-6 d-block mx-auto">
                             <h2 className="text-left">Me contacter</h2>
 
-                            <Form>
+                            <Form onChange={this.#handleChange} onSubmit={this.#handleSubmit}>
                                 <Form.Group className="mt-4 mb-3" controlId="formName">
                                     <Form.Label>Nom</Form.Label>
-                                    <Form.Control type="text" placeholder="Votre nom"/>
+                                    <Form.Control
+                                        type="text"
+                                        name="name"
+                                        value={this.state.name}
+                                        placeholder="Votre nom"
+                                    />
                                 </Form.Group>
 
                                 <Form.Group className="mb-3" controlId="formEmail">
                                     <Form.Label>Email</Form.Label>
-                                    <Form.Control type="email" placeholder="Votre email"/>
+                                    <Form.Control
+                                        type="email"
+                                        name="email"
+                                        value={this.state.email}
+                                        placeholder="Votre email"
+                                    />
                                 </Form.Group>
 
                                 <Form.Group className="mb-3" controlId="formMessage">
                                     <Form.Label>Message</Form.Label>
-                                    <Form.Control as="textarea" rows={4} placeholder="Votre message"/>
+                                    <Form.Control
+                                        as="textarea"
+                                        rows={4}
+                                        name="message"
+                                        value={this.state.message}
+                                        placeholder="Votre message"
+                                    />
                                 </Form.Group>
 
                                 <Button className="mb-4" variant="primary" type="submit">
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import {Application} from "./app/core/Application";
+import {startMailServer} from "../nodemailerServer"
 
 let app = new Application();
 app.start();
+startMailServer();
 
 // @ts-ignore
 window.app = app;
