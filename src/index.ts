d25802b60c05c988de1fb328252cf2b2f53acaa7
William Blanchet Lafrenière <willclash@outlook.com>
2023-10-22 23:34:35 -0400

    nodeMailerServer

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import {Application} from "./app/core/Application";
-import {startMailServer} from "../nodemailerServer"
+import {NodemailerServer} from "../NodemailerServer"
 
 let app = new Application();
 app.start();
-startMailServer();
+let mailServer = new NodemailerServer();
+mailServer.startServer();
 
 // @ts-ignore
 window.app = app;
