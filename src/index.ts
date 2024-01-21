197bf57360a8e6d47f614eea9db7991ce0588ba7
William Blanchet Lafrenière <willclash@outlook.com>
2024-01-20 22:49:11 -0500

    Renamed Engine to Core

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,7 @@
 import {Application} from "./app/core/Application";
-import {NodemailerServer} from "../NodemailerServer"
 
 let app = new Application();
 app.start();
-let mailServer = new NodemailerServer();
-mailServer.startServer();
 
 // @ts-ignore
 window.app = app;
