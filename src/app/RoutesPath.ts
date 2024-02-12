7218521215d50af3e8f4c5069a7efc4b3f72a53d
William Blanchet Lafrenière <willclash@outlook.com>
2024-02-12 04:45:28 +0000

    gh-pages

--- src/app/RoutesPath.ts
+++
@@ -1,9 +0,0 @@
-export enum RoutesPath {
-    INDEX = "/",
-    PROJETS = "/projets",
-    EXPERIENCE = "/experience",
-    ETUDES = "/etudes",
-    REFERENCES = "/references",
-    APROPOS = "/apropos",
-    CONTACT = "/contact",
-}
--- src/app/constants/Global.ts
+++
@@ -1,2 +0,0 @@
-export let APP_NAME: string = "WBL-WebApp"
-export let BASE_URL: string | undefined = process.env.NODE_ENV === 'development' ? '/' : '/WBL-WebApp/'
