b02fcb43da667b60496c652b6830c23ba6b9450d
Villy <willclash@outlook.com>
2025-02-02 01:35:44 -0500

    ajout CIAUL

--- src/app/pages/Groupes.tsx
+++ src/app/pages/Groupes.tsx
@@ -4,6 +4,7 @@ import {APP_NAME} from "../constants/Global";
 import {Application} from "../core/Application";
 import Dynamiques from "../deps/images/Dynamiques.png";
 import Eperviers from "../deps/images/Eperviers.png";
+import CIAUL from "../deps/images/CIAUL2.png";
 import ClubBadmintonQuebec from "../deps/images/ClubBadmintonQuebec.png";
 import SeigneursNaguerre from "../deps/images/SeigneursNaguerre.png";
 import Grimoire from "../deps/images/Grimoire.png";
@@ -39,6 +40,14 @@ class group extends React.Component<WithTranslation> {
                     </div>
                 </Row>
                 <h2>{t("clubs")}</h2>
+                <Row className="mb-4">
+                    <Image className="img-responsive" src={CIAUL} alt={t("group_cia_alt")}/>
+                    <div className="mt-2">
+                        <h3>{t("club_cia_title")}</h3>
+                        <p>{t("club_cia_description")}</p>
+                        <p>{t("club_cia_period")}</p>
+                    </div>
+                </Row>
                 <Row className="mb-4">
                     <Image className="img-responsive" src={ClubBadmintonQuebec} alt={t("group_badmintonquebec_alt")}/>
                     <div className="mt-2">
